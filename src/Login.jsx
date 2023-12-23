ebd9eeadad811661cd1a37bc4a486f4b382380d4
Michal <korczewskimichal@gmail.com>
2023-12-23 20:19:11 +0100

    I have made a login page that will log to console. The next step is some form of authentication. Once that is complete, I will be able to make the SQL for the page, and then logging in through the admin panel will allow me to create custom SQL prepared statements that I can use to add cars to the page easily without going into the database manually and changing stuff. Once the SQL database is made we can work on displaying it on the page.

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,25 @@
+import React, { useState } from "react";
+import './Login.css';
+
+
 export const Login = () =>{
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username)
+    }
+
     return(
-        <form>
+        <div className="auth-form-container">
+        <form className="login-form"onSubmit={handleSubmit}>
         <label for="username: ">Username: </label>
-        <input type="Username" placeholder="example: JohnAppleseed123" id="username" name="username"/>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} type="Username" placeholder="example: JohnAppleseed123" id="username" name="username"/>
         <label for="password: ">Password: </label>
-        <input type="Password" placeholder="Password" id="password" name="password"/>
+        <input value={password} onChange={(e) => setPassword(e.target.value)}type="Password" placeholder="Password" id="password" name="password"/>
+        <button type= "submit">Log in</button>
         </form>
+        </div>
     )
 }
--- src/adminPanel.js
+++ src/adminPanel.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import './Login';
 import { Login } from './Login';
 
